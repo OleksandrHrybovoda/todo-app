ecda5e8f513c54881158883ddb6c3ad345a17d44
DESKTOP-48MU8MN\User <oleksandr.grybovoda@embrox.com>
2021-04-20 18:45:57 +0300

    TAO-17 created filter-input to find user by id, firstName or lastName

--- src/app/modules/users/components/users-list/users-list.component.ts
+++ src/app/modules/users/components/users-list/users-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { User } from 'src/app/core/models/user.model';
@@ -10,7 +11,7 @@ import { UsersProvider } from 'src/app/services/users.provider';
   styleUrls: ['./users-list.component.sass']
 })
 export class UsersListComponent implements OnInit, OnDestroy {
-  public users: User[];
+  public users = new MatTableDataSource();
   public displayedColumns: string[] = ['id', 'firstName', 'lastName', 'shortcut', 'age', 'gender', 'email', 'login'];
 
   private readonly destroy$ = new Subject();
@@ -21,15 +22,33 @@ export class UsersListComponent implements OnInit, OnDestroy {
     this.init();
   }
 
+  public applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.users.filter = filterValue.trim().toLowerCase();
+  }
+
   private init(): void {
     this.prepareUsersToShow();
+    this.activateFilterPredicate();
+  }
+
+  public activateFilterPredicate(): void {
+    this.users.filterPredicate = (data: User, filterValue: string): boolean => {
+      return data.firstName
+        .trim()
+        .toLocaleLowerCase().indexOf(filterValue.trim().toLocaleLowerCase()) >= 0 || data.lastName
+        .trim()
+        .toLocaleLowerCase().indexOf(filterValue.trim().toLocaleLowerCase()) >= 0 || data.id.toString()
+        .trim()
+        .toLocaleLowerCase().indexOf(filterValue.trim().toLocaleLowerCase()) >= 0;
+    };
   }
 
   private prepareUsersToShow(): void {
     this.usersProvider.getUsers()
       .pipe(takeUntil(this.destroy$))
-      .subscribe(tasks => {
-        this.users = tasks;
+      .subscribe(users => {
+        this.users = new MatTableDataSource(users);
       });
   }
 
