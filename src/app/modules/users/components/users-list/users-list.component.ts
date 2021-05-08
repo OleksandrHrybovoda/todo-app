5a80beadcef392d8c36bddf470b96a2f693a38e9
DESKTOP-48MU8MN\User <oleksandr.grybovoda@embrox.com>
2021-05-08 15:14:15 +0300

    TAO-25 paginated users via server

--- src/app/modules/users/components/users-list/users-list.component.ts
+++ src/app/modules/users/components/users-list/users-list.component.ts
@@ -22,7 +22,7 @@ export class UsersListComponent extends EntitiesListBaseClass implements OnInit,
 
   public users: MatTableDataSource<User> = new MatTableDataSource();
   public length: number;
-  public pageSize: number = 20;
+  public pageSize: number = 5;
   public currentPage: number = 0;
   public pageEvent: PageEvent;
   public pageSizeOptions: number[] = [5, 10, 25, 100];
@@ -42,7 +42,7 @@ export class UsersListComponent extends EntitiesListBaseClass implements OnInit,
 
   public pageEventHandler(event: PageEvent): void {
     this.pageEvent = event;
-    console.log(event);
+    this.prepareUsersToShow(this.pageEvent.pageIndex, this.pageEvent.pageSize);
   }
 
   private subscribeToUserCreation(): void {
