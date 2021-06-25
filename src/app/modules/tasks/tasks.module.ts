04ef8fa7c50d2eb3b7003af92a61c0da0e1c87db
DESKTOP-48MU8MN\User <oleksandr.grybovoda@embrox.com>
2021-06-25 12:20:10 +0300

    TAO-60 removed core folder and moved all files to auth module

--- src/app/modules/tasks/tasks.module.ts
+++ src/app/modules/tasks/tasks.module.ts
@@ -1,6 +1,6 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { TokenInterceptor } from 'src/app/core/interceptors/token.interceptor';
+import { TokenInterceptor } from 'src/app/modules/auth/interceptors/token.interceptor';
 import { ComponentsModule } from './components/components.module';
 import { ServicesModule } from './services/services.module';
 import { TasksRoutingModule } from './tasks-routing.module';
